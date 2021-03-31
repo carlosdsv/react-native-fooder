4331de0deec97f104b87683e89b3ae49142dfceb
carlosdsv <carlosdsv@gmail.com>
2021-03-31 19:49:28 +0200

    delete console.logs

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import { YELP_API_KEY, YELP_API_URL } from '@env';
-console.log(YELP_API_URL);
-console.log(YELP_API_KEY);
 
 export default axios.create({
   baseURL: `${YELP_API_URL}`,
